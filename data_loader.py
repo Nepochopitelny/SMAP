912a7a3b6ef8bca6cd2b3339f6f777c91d060e9e
Roman Sedivy <roman.sedivy@plus4u.net>
2023-12-14 13:01:18 +0100

    Added stemming and lemmantization methods

--- data_loader.py
+++ data_loader.py
@@ -1,32 +1,45 @@
-import pandas
-from nltk.stem import PorterStemmer
+from pandas import read_csv, DataFrame
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+nltk.download('wordnet')
 
 def getDataFrame(file_name, separator):
-    data = pandas.read_csv(file_name, sep=separator, header=None, names=['label', 'text'])
-    return pandas.DataFrame(data)
+    data = read_csv(file_name, sep=separator, header=None, names=['label', 'text'])
+    return DataFrame(data)
+    
+def preprocessOnlyRemovingStopWords(text):
+    stop_words = set(stopwords.words('english'))
+    words = word_tokenize(text)
+    words = [word for word in words if not word.lower() in stop_words]
+    return ' '.join(words)
 
-#Ocisteni textu
-def preprocess(text):
+def preprocessWithStemming(text):
     stop_words = set(stopwords.words('english'))
     porter_stemmer = PorterStemmer()
     words = word_tokenize(text)
-    words = [porter_stemmer.stem(word.lower()) for word in words if word.isalpha() and word.lower() not in stop_words]
+    words = [porter_stemmer.stem(word.lower()) for word in words if word.lower() not in stop_words]
+    return ' '.join(words)
+
+def preprocessWithLemmantization(text):
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    words = word_tokenize(text)
+    words = [lemmatizer.lemmatize(word.lower()) for word in words if word.lower() not in stop_words]
     return ' '.join(words)
 
 
-def splitAndVectorizeData(data_frame, test_size):
-    processed_text = data_frame['text'].apply(preprocess)
+def splitAndVectorizeData(data_frame, test_size, vectorizer):
+    processed_text = data_frame['text'].apply(preprocessWithLemmantization)
     X_train, X_test, y_train, y_test = train_test_split(processed_text, data_frame['label'], test_size=test_size, random_state=5)
-    vectorizer = CountVectorizer()
     X_train_vectorized = vectorizer.fit_transform(X_train)
     X_test_vectorized = vectorizer.transform(X_test)
     return X_train_vectorized, X_test_vectorized, y_train, y_test
 
-def getVectorizedDataFromDataFrame(file_name, separator, test_size):
+
+def getVectorizedDataFromDataFrame(file_name, separator, test_size, vectorizer):
     data_frame = getDataFrame(file_name, separator)
-    return splitAndVectorizeData(data_frame, test_size)
+    return splitAndVectorizeData(data_frame, test_size, vectorizer)
 
--- machine_algorithms.py
+++ machine_algorithms.py
@@ -1,31 +1,53 @@
 from data_loader import *
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.ensemble import RandomForestClassifier
 
+def applyFutureSelection(X_train_vec, X_test_vec, y_train, futureSelectionAlgorithm):
+    print("Applying chi squared feature selection")
+    selector = SelectKBest(futureSelectionAlgorithm, k=8)
+    X_train = selector.fit_transform(X_train_vec, y_train)
+    X_test = selector.transform(X_test_vec)
+    return X_train, X_test
 
-def testAlgorithm(algorithm, X_train_vec, X_test_vec, y_train, y_test):
+def testMLAlgorithm(algorithm, X_train_vec, X_test_vec, y_train, y_test, futureSelectionAlgorithm=None):
+    if(futureSelectionAlgorithm != None):
+        X_train_vec, X_test_vec = applyFutureSelection(X_train_vec, X_test_vec, y_train, futureSelectionAlgorithm)
     algorithm.fit(X_train_vec, y_train)
     predictions = algorithm.predict(X_test_vec)
-    print("Class report: \n", classification_report(y_test, predictions))
-
-def applyChiSquaredFeatureSelection(X_train_vec, X_test_vec, y_train):
-    print("Applying chi squared feature selection")
-    selector = SelectKBest(chi2, k=5)
-    X_train_chi = selector.fit_transform(X_train_vec, y_train)
-    X_test_chi = selector.transform(X_test_vec)
-    return X_train_chi, X_test_chi
+    print("Class report: \n", classification_report(y_test, predictions, digits=4))
+    print("Confusion matrix: \n", confusion_matrix(y_test, predictions))
 
 
+X_train_vec, X_test_vec, y_train, y_test = getVectorizedDataFromDataFrame('spam_NStugard.csv', ',',0.2, TfidfVectorizer())
+#X_train_vec, X_test_vec, y_train, y_test = getVectorizedDataFromDataFrame('spam_v2.csv', ';',0.2, TfidfVectorizer())
+print("Testing results with TFID Vectorizer")
 
-X_train_vec, X_test_vec, y_train, y_test = getVectorizedDataFromDataFrame('spam_NStugard.csv', ',',0.2)
-
-
-X_train_vec, X_test_vec = applyChiSquaredFeatureSelection(X_train_vec, X_test_vec, y_train)
-
-print("Testing KNeighborsClassifier algorithm")
-testAlgorithm(KNeighborsClassifier(), X_train_vec, X_test_vec, y_train, y_test)
+print("Testing RandomForestClassifier algorithm")
+testMLAlgorithm(RandomForestClassifier(), X_train_vec, X_test_vec, y_train, y_test)
 
 print("Testing MultinomialNB algorithm")
-testAlgorithm(MultinomialNB(), X_train_vec, X_test_vec, y_train, y_test)
+testMLAlgorithm(MultinomialNB(), X_train_vec, X_test_vec, y_train, y_test)
+
+print("Testing support vector algorithm")
+testMLAlgorithm(SVC(), X_train_vec, X_test_vec, y_train, y_test)
+
+
+X_train_vec, X_test_vec, y_train, y_test = getVectorizedDataFromDataFrame('spam_NStugard.csv', ',',0.2, CountVectorizer())
+#X_train_vec, X_test_vec, y_train, y_test = getVectorizedDataFromDataFrame('spam_v2.csv', ';',0.2, CountVectorizer())
+print("##########################################################################################")
+print("##########################################################################################")
+print("##########################################################################################")
+print("Testing with CountVectorizer")
+print("Testing RandomForestClassifier algorithm")
+testMLAlgorithm(KNeighborsClassifier(), X_train_vec, X_test_vec, y_train, y_test)
+
+print("Testing MultinomialNB algorithm")
+testMLAlgorithm(MultinomialNB(), X_train_vec, X_test_vec, y_train, y_test)
+
+print("Testing support vector algorithm")
+testMLAlgorithm(SVC(), X_train_vec, X_test_vec, y_train, y_test)
